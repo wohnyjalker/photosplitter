22feb3e9ec1a46c6df6cb8673714f47fc4b858ad
wohnyjalker <wohnyjalker@gmail.com>
2019-08-30 13:59:09 +0200

    fix variables name in accordance with PEP8

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from PIL import Image
-import os, sys
+import os
+import sys
 
 def print_help():
     print("Takes one argument: .jpg file\nUsage 'python script.py file.jpg'\nTo split all .jpg photos in current directory first:\n'chmod +x runscript' next './runscript'")
@@ -11,17 +12,17 @@ if (len(sys.argv) < 2) or (len(sys.argv) >= 3):
 if (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
     print_help()   
 
-dirName = 'folder'
+dir_name = 'folder'
 counter = 1
-filePath = os.getcwd() + '/' + dirName + '/'
+file_path = os.getcwd() + '/' + dir_name + '/'
 boxes = [(16, 201, 601, 591), (16, 600, 600, 990), (16, 1000, 600, 1390)]
 
 try:
     # Create target Directory
-    os.mkdir(dirName)
-    print("Directory", dirName,  "created.") 
+    os.mkdir(dir_name)
+    print("Directory", dir_name,  "created.") 
 except FileExistsError:
-    print("Directory", dirName, "already exists.")
+    print("Directory", dir_name, "already exists.")
     pass
 
 try:
@@ -30,17 +31,17 @@ except FileNotFoundError:
     print(sys.argv[1], "no such file.")
     exit()
 
-sourceFileName = sys.argv[1].replace('.jpg','')
+source_file_name = sys.argv[1].replace('.jpg','')
 print("Source file:", sys.argv[1])
 
 for box in boxes:
-    name = sourceFileName + "_img" + str(counter) + ".jpg"
-    imageFinalPath = filePath + name
+    name = source_file_name + "_img" + str(counter) + ".jpg"
+    image_final_path = file_path + name
 
     region = im.crop(box)
-    imageFinal = Image.new("RGB", (585, 390), 0)
-    imageFinal.paste(region)
-    imageFinal.save(imageFinalPath, quality=95)
+    image_final = Image.new("RGB", (585, 390), 0)
+    image_final.paste(region)
+    image_final.save(image_final_path, quality=95)
     print(name, "saved.")
     counter += 1
 
